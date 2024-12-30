4719112e64c2d8b392bbdf174ee17a54763ec024
bruno-venturini <brunofreloventurini@outlook.com>
2024-12-30 19:26:44 -0300

    Criando cadastro roupa

--- src/main/java/com/brilhatte/app/controllers/RoupaController.java
+++ src/main/java/com/brilhatte/app/controllers/RoupaController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +36,11 @@ public class RoupaController {
         });
 
         return ResponseEntity.ok(roupasDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<RoupaDTO> save(@RequestBody RoupaDTO roupaDTO) {
+        Roupa roupa = roupaService.save(RoupaDTO.toEntity(roupaDTO));
+        return ResponseEntity.ok(RoupaDTO.fromEntity(roupa));
     }
 }
